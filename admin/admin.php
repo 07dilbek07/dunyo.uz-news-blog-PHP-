a4bea338cb174391485937adb503db59fd1bb466
Dilbek Dustmurodov <dilbekdustmurodov2002@gmail.com>
2022-10-14 15:59:06 +0500

    mm

--- admin/admin.php
+++ admin/admin.php
@@ -14,7 +14,8 @@ $categoryList = Array("world", "money", "culture", "sport");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AdminPanel</title>
+    <link rel="icon" type="image/x-icon" href="../image/favicon.ico">
+    <title>AdminDunyo.uz</title>
 </head>
 
 <body>
